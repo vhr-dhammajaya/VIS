7ce45d64101e6571aa364cf68e03fbf24e7cbbed
Yinsi Rio <yinsi.rio@gmail.com>
2019-12-26 14:23:06 +0700

    Delete backend

--- backend/src/main/java/com/skopware/vdjvis/backend/config/Config.java
+++
@@ -1,8 +0,0 @@
-package com.skopware.vdjvis.backend.config;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.skopware.javautils.dropwizard.BaseConfiguration;
-import io.dropwizard.db.DataSourceFactory;
-
-public class Config extends BaseConfiguration {
-}
