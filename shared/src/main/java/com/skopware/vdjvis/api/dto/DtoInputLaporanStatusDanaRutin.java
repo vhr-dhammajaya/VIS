ada8be2feaa50ff0d582f582498492b388499fea
Yinsi Rio <yinsi.rio@gmail.com>
2019-12-26 14:25:36 +0700

    Delete unused classes

--- shared/src/main/java/com/skopware/vdjvis/api/dto/DtoInputLaporanStatusDanaRutin.java
+++
@@ -1,7 +0,0 @@
-package com.skopware.vdjvis.api.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class DtoInputLaporanStatusDanaRutin {
-    @JsonProperty public String idUmat;
-}
